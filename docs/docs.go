85a5946866d441f7c63ba2fdca36028a397ce547
JPratama7 <josepratama080@gmail.com>
2023-06-19 08:53:02 +0700

    find location from text

--- docs/docs.go
+++ docs/docs.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"google.golang.org/api/docs/v1"
 	"log"
+	"strings"
 )
 
 type GoogleDocs struct {
@@ -23,6 +24,28 @@ func (gdocs *GoogleDocs) FindAndReplace(docId string, request ...*docs.Request) 
 	_, err = gdocs.srv.Documents.BatchUpdate(docId, &docs.BatchUpdateDocumentRequest{Requests: request}).Do()
 	if err != nil {
 		return
+	}
+
+	return
+}
+
+func (gdocs *GoogleDocs) FindTextLocation(docId string, pattern string) (loc docs.Location, err error) {
+	file, err := gdocs.srv.Documents.Get(docId).Do()
+
+	for _, element := range file.Body.Content {
+		if element.Paragraph == nil {
+			continue
+		}
+		for _, run := range element.Paragraph.Elements {
+			if run.TextRun == nil {
+				continue
+			}
+			if !strings.ContainsAny(run.TextRun.Content, pattern) {
+				continue
+			}
+			loc.Index = run.StartIndex
+			break
+		}
 	}
 
 	return
---
+++ example/findtextloc/test.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	gwrp "github.com/JPratama7/gwrap"
+	gdocs "github.com/JPratama7/gwrap/docs"
+	"google.golang.org/api/docs/v1"
+	"google.golang.org/api/drive/v2"
+	"google.golang.org/api/option"
+
+	"log"
+)
+
+func replaceTextWithTable(docID string, requests []*docs.Request) error {
+	srv, err := docs.NewService(context.Background())
+	if err != nil {
+		return err
+	}
+
+	_, err = srv.Documents.BatchUpdate(docID, &docs.BatchUpdateDocumentRequest{
+		Requests: requests,
+	}).Do()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	ctx := context.Background()
+	filepath := "./credentials.json"
+	// If modifying these scopes, delete your previously saved token.json.
+	//b, err := os.ReadFile(filepath)
+	//if err != nil {
+	//	log.Fatalf("Unable to read client secret file: %v", err)
+	//}
+
+	// Parse the client secret file and configure the OAuth2 client
+	//config, err := google.ConfigFromJSON(b, drive.DriveScope, drive.DriveReadonlyScope, docs.DocumentsScope, docs.DocumentsReadonlyScope)
+	//if err != nil {
+	//	log.Fatalf("Unable to parse client secret file to config: %v", err)
+	//}
+	cfg, err := gwrp.NewGoogleConfig(filepath, drive.DriveScope, drive.DriveReadonlyScope, docs.DocumentsScope, docs.DocumentsReadonlyScope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v\n", err)
+		return
+	}
+	client := gwrp.GetClient(cfg, "token.json")
+
+	srvDocs, err := docs.NewService(ctx, option.WithHTTPClient(client))
+	if err != nil {
+		log.Fatalf("Unable to retrieve Docs client: %v", err)
+		return
+	}
+
+	gdrService := gdocs.NewGoogleDocs(srvDocs)
+	//gdcsService := gdocs.NewGoogleDocs(srvDocs)
+	// Prints the title of the requested doc:
+	// https://docs.google.com/document/d/195j9eDD3ccgjQRttHhJPymLJUCOUjs-jmwTrekvdjFE/edit
+
+	loc, err := gdrService.FindTextLocation("1s6cIpltLDhwu2nxyORPV-A1Q0nvlq8yz0onLLR1D0BM", "{{CROT}}")
+
+	fmt.Printf("crooot : %s\n", err)
+	fmt.Printf("loc : %+v\n", loc)
+}
